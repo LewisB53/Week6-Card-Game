5cb0323095487c641508254bfd08b2e02730fdcd
Lewis <lewisburns630@googlemail.com>
2017-05-20 01:28:34 +0100

    Assigned value to enum, need to test

--- cards/CardNumber.java
+++ cards/CardNumber.java
@@ -1,17 +1,26 @@
 package cards;
 
 public enum CardNumber{
- ACE,
- TWO,
- THREE,
- FOUR,
- FIVE,
- SIX,
- SEVEN,
- EIGHT,
- NINE,
- TEN,
- JACK,
- QUEEN,
- KING
+ ACE (1),
+ TWO (2),
+ THREE (3),
+ FOUR (4),
+ FIVE (5),
+ SIX (6),
+ SEVEN (7),
+ EIGHT (8),
+ NINE (9),
+ TEN (10),
+ JACK (11),
+ QUEEN (12),
+ KING(13);
+
+private final int value;
+
+  CardNumber(final int newValue) {
+    value = newValue;
+    }
+
+public int getValue() { return value; }
+  
 }
