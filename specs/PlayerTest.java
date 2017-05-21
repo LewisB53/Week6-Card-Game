c91d431e1fdeb62cbabf1bc24cd3040f5523cbc6
Lewis <lewisburns630@googlemail.com>
2017-05-21 08:48:13 +0100

    Player can get card

---
+++ specs/PlayerTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import cards.*;
+
+public class PlayerTest {
+
+  Card card;
+  
+  @Before 
+  public void before(){
+    card = new Card(CardNumber.FIVE , CardSuit.SPADES);
+  }
+
+  @Test
+  public void canGetCard(){
+    assertEquals(CardNumber.FIVE, card.getCardNumber());
+  }
+
+
+
+}
