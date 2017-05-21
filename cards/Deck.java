9a8c89959eb63668f3b819002eff6dda6b3f75d6
Lewis <lewisburns630@googlemail.com>
2017-05-21 09:07:15 +0100

    Deck can get card

---
+++ cards/Deck.java
@@ -0,0 +1,14 @@
+package cards;
+import behaviours.*;
+import java.util.*;
+
+
+public class Deck {
+  
+  private ArrayList<Holdable> deck;
+
+  public Deck(String playerName){
+    this.deck = new ArrayList<Holdable>();
+  }
+
+}
